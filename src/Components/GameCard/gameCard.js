ac1f0b811e5b7a7bf94e133cdd9ebc0306a16e80
mrderrickguy <zakiahdg@gmail.com>
2018-05-17 12:57:46 -0500

    yarn build

--- src/Components/GameCard/gameCard.js
+++ src/Components/GameCard/gameCard.js
@@ -3,7 +3,7 @@ import "./gameCard.css";
 
 const GameCard = props => (
     <div className="herocard">
-      <div className="img-container" dataid={props.id} onClick={() => props.randomDisplay(props.id)}>
+      <div className="img-container" gameid={props.id} onClick={() => props.randomDisplay(props.id)}>
           <img src={props.image} alt={props.name}/>
       </div>  
     </div>
